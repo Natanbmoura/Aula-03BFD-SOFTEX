ae1c9bb9861522d401713f245d05b6a509dc5c10
Natanbmoura <natan.bmoura1@gmail.com>
2025-09-04 17:01:32 -0300

    aula9

---
+++ aula9/aula9.js
@@ -0,0 +1,11 @@
+//Atributos simples = Nome(aluno), idade, CPF    
+//Atributos compostos = endereço, rua, bairro, CEP, CIDADE, Estado, Complemento 
+//Monovalorado = Matricula, Código, CPF
+//Multivalorado = Telefone
+
+//\n--------------------------------------------------
+
+//Atributos simples = Nome(Funcionário), Função(Funcionário), Idade(Funcionário), Cidade(Funcionário) Nome(Filial)
+//Atributos compostos = Endereço, Rua, Bairro, CEP, Cidade, Complemento, Razão Social, 
+//Monovalorado = CNPJ, Código(Funcionário), Código (Filial)
+//Multivalorado = Telefone(Filial)
